2415d7a2f83608f69eaf8f9b64659f1645580b11
yunzehui <1515440680@qq.com>
2022-03-28 16:21:18 +0800

    update

--- src/mock/login/mock.js
+++ src/mock/login/mock.js
@@ -1,26 +1,26 @@
 let Mock = require('mockjs')
 
-Mock.mock('/api/login','post',function(config){
+Mock.mock('/api/login', 'post', function (config) {
    // console.log(config) //参数
    let obj = JSON.parse(config.body)
    let name = obj.name
    let password = obj.password
-   if(name === '12345' && password === '0000'){
+   if (name === '12345' && password === '0000') {
       return {
-         status:200,
-         message:'success',
-         data:{
-            name:'12345',
-            password:0000,
-            token:'一条瞎写的token'
+         status: 200,
+         message: 'success',
+         data: {
+            name: '12345',
+            password: '0000',
+            token: '一条瞎写的token'
          }
       }
-   }else {
+   } else {
       return {
-         status:200,
-         message:'fail',
-         data:{
-            message:'用户密码错误'
+         status: 200,
+         message: 'fail',
+         data: {
+            message: '用户密码错误'
          }
       }
    }
